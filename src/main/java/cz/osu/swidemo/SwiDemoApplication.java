e36e78a0e8c483da6bc0446fdbbc0d7d1bd2c0a2
F23413 <sukona21@osu.cz>
2026-02-26 16:34:19 +0100

    bob attempt

--- src/main/java/cz/osu/swidemo/SwiDemoApplication.java
+++ src/main/java/cz/osu/swidemo/SwiDemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SwiDemoApplication {
 
     public static void main(String[] args) {
-
+        //bob has been called to the meeting
         SpringApplication.run(SwiDemoApplication.class, args);
     }
 }
